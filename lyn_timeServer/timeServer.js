ab28586878f96ee71512e28c52e0efc2ff164086
LivBandeira <bandeira.liv@gmail.com>
2017-11-12 17:36:10 -0800

    Create timeServer.js

---
+++ lyn_timeServer/timeServer.js
@@ -0,0 +1,14 @@
+let net = require('net')
+
+function date(i) { return i < 10 ? '0' + i : i }
+
+let server = net.createServer(function(socket) {
+  d = new Date()
+  s = d.getFullYear() + "-" +
+    date(d.getMonth() + 1) + "-" +
+    date(d.getDate()) + " " + 
+    date(d.getHours()) + ":" +
+    date(d.getMinutes()) + "\n"; +
+  socket.end(s);
+})
+server.listen(process.argv[2])
