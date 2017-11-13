a7c8c0ef8d829e64d916ef1db38430da97b16fb1
LivBandeira <bandeira.liv@gmail.com>
2017-11-12 19:55:40 -0800

    Create HTTPuppercaserer.js

---
+++ lyn_HTTPuppercaserer/HTTPuppercaserer.js
@@ -0,0 +1,13 @@
+let http = require('http')
+let map = require('through2-map')
+
+let upC = map(function(chunk) {
+  return chunk.toString().toUpperCase();
+})
+
+let server = http.createServer(function(req, res) {
+  if (req.method == 'POST') {
+    req.pipe(upC).pipe(res)
+  }
+})
+server.listen(process.argv[2])
