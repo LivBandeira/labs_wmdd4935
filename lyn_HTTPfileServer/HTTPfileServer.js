2897742e374c89f8238d5e2f6c5d9265d6e7d5bb
LivBandeira <bandeira.liv@gmail.com>
2017-11-16 12:28:22 -0800

    Update HTTPfileServer.js

--- lyn_HTTPfileServer/HTTPfileServer.js
+++ lyn_HTTPfileServer/HTTPfileServer.js
@@ -1,5 +1,5 @@
-let fs = require('fs')
-let http = require('http')
+const fs = require('fs')
+const http = require('http')
 let file = process.argv[3]
 
 server = http.createServer(function(req, res) {
